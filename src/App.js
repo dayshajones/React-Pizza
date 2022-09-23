92cd36d3a79c73d027de2ef9741dfacc85547323
“Daysha <+>
2022-09-23 13:11:07 -0700

    commit tree 4d5769eb880ac6bf5030c2d5c512b600ceac7d98

--- src/App.js
+++ src/App.js
@@ -3,12 +3,67 @@ import Header from './components/Header'
 import PizzaForm from './components/PizzaForm'
 import PizzaList from './containers/PizzaList'
 class App extends Component {
+
+  state = {
+    pizzas: [],
+    pizzaToEdit: {
+      id: null,
+      vegetarian: false,
+      topping: '',
+      size: ''
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/pizzas")
+    .then((resp) => resp.json())
+    .then(pizzas => this.setState({pizzas}))
+}
+
+editPizza = (pizza) => {
+  this.setState({pizzaToEdit: pizza})
+}
+
+onChange = (event) => {
+  const target = event.target
+  const value = target.value
+  const name = target.name
+  this.setState({
+    pizzaToEdit: {...this.state.pizzaToEdit, [name]: value }
+  })
+}
+
+onSubmit = () => {
+  const {pizzas, pizzaToEdit} = this.state
+  fetch(`http://localhost:3000/pizzas/${pizzaToEdit.id}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({...pizzaToEdit})
+  })
+  .then(res=> res.json())
+  .then(newPizza => {
+      let pizzaIndex = pizzas.findIndex(pizza => pizza.id === newPizza.id)
+      let newPizzas = [...pizzas]
+      newPizzas[pizzaIndex] = newPizza
+      this.setState({pizzas: newPizzas,
+        pizzaToEdit: {
+          id: null,
+          vegetarian: false,
+          topping: "",
+          size: ""
+        }
+      })
+    })
+}
+
   render() {
     return (
       <Fragment>
         <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <PizzaForm {...this.state.pizzaToEdit} onSubmit={this.onSubmit} onChange={this.onChange}/>
+        <PizzaList pizzas={this.state.pizzas} editPizza={this.editPizza}/>
       </Fragment>
     );
   }
